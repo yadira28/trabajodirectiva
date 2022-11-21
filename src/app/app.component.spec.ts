e5b53ea5065b5f075412945ad36f1a1ce30f247f
yadira28 <yadira.vazquezsantana@cbtis72.edu.mx>
2022-11-21 01:18:07 -0600

    iniciando

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'trabajoDirectivaApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('trabajoDirectivaApp');
+    expect(app.titulo).toEqual('trabajoDirectivaApp');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de Usuarios';
 }
