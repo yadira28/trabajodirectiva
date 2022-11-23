8f2f2f3c551c6fb38f8a6bb87150e955c435ff59
yadira28 <yadira.vazquezsantana@cbtis72.edu.mx>
2022-11-23 14:47:58 -0600

    directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   titulo = 'Registro de Usuarios';
+  mensaje= "";
+  registrado=false;
+  nombre:string="";
+  apellido:string="";
+  entradas: object[];
+
+  constructor(){
+    this.entradas=[
+      {titulo:"Python cada dia mas presente"},
+      {titulo:"Java presente desde hace mas de 20 años"},
+      {titulo:"JavaScript cada dia mas funcional"},
+      {titulo:"Kotlin potencia pata tus apps"},
+      {titulo:"¿Donde quedo pascal?"},
+    ]
+
+  }
+
+  registrarUsuario(){
+    this.registrado=true
+    this.mensaje="usuario registrado con exito"
+  }
 }
